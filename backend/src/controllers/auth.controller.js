80b6903a47f34d6e9c283db9ac05282052dace58
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2026-03-08 14:18:38 +0100

    Add validation for missing fields in `login` controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -67,6 +67,10 @@ export const signUp = async (req, res) => {
 export const login = async (req, res) => {
     const {email, password} = req.body;
 
+    if (!email || !password) {
+        return res.status(400).json({message: "Please fill all fields"});
+    }
+
     try {
         const user = await User.findOne({email})
         if (!user) return res.status(400).json({message: "Invalid email or password"});
