612a791af9e592a325d973670e8efb77ac5e121d
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2026-03-09 09:34:45 +0100

    Add message routes for fetching and sending messages
    - Added `getMessagesByUserId` and `sendMessage` controllers to handle user message retrieval and new message creation.
    - Integrated Cloudinary for optional message image uploads in `sendMessage` controller.
    - Defined new `GET /:id` and `POST /send/:id` routes in `message.route.js` with `protectRoute` middleware for authentication.

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Message from '../models/Message.js';
 import User from '../models/User.js';
+import cloudinary from "../lib/cloudinary.js";
 
 export const getAllContacts = async (req, res) => {
     try {
@@ -9,6 +10,53 @@ export const getAllContacts = async (req, res) => {
         res.status(200).json(filteredUsers);
     } catch (error) {
         console.error(`Error fetching contacts: ${error.message}`);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
+
+export const getMessagesByUserId = async (req, res) => {
+    try {
+        const myId = req.user._id;
+        const {id: userToCharId} = req.params;
+
+        const message = await Message.find({
+            $or: [
+                {sender: myId, receiverId: userToCharId},
+                {sender: userToCharId, receiverId: myId},
+            ]
+        })
+
+        res.status(200).json(message);
+
+    } catch (error) {
+        console.error(`Error in getMessages controller: ${error.message}`);
+        res.status(500).json({message: "Internal server error"});
+    }
+};
+export const sendMessage = async (req, res) => {
+    try {
+        const {text, image } = req.body;
+        const {id: receiverId} = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+
+        if (image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        })
+
+        await newMessage.save();
+        res.status(201).json(newMessage);
+    } catch (error) {
+        console.error(`Error in sendMessage controller: ${error.message}`);
         res.status(500).json({message: "Internal server error"});
     }
 }
