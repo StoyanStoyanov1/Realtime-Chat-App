d75665522128758abdb6513db23d380380b91236
StoyanStoyanov1 <stoyan.stoyanov93@yahoo.com>
2026-03-10 20:16:32 +0100

    Add authentication flow and CORS support
    - Implemented authentication check (`checkAuth`) in `useAuthStore` with Zustand.
    - Added navigation guards using `react-router` to redirect based on authentication state.
    - Introduced a `PageLoader` component for improved app loading UX.
    - Installed and configured `cors` in the backend to enable cross-origin requests.
    - Updated dependencies to include `lucide-react` for loading indicators.

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,12 +1,19 @@
 import {create} from 'zustand';
+import {axiosInstance} from "../lib/axios.js";
 
 export const useAuthStore = create(set => ({
     authUser: null,
-    isLoggedIn: false,
-    isLoading: false,
+    isCheckingAuth: true,
 
-    login: () => {
-        console.log("Login");
-        set({isLoggedIn: true, isLoading: true});
-    }
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get('/auth/check');
+            set({authUser: res.data});
+        } catch (error) {
+            console.error('Error in authCheck:', error);
+            set({authUser: null});
+        } finally {
+            set({isCheckingAuth: false});
+        }
+    },
 }));
